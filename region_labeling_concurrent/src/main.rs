d591c46f3cef387020be76d0f4cd5ce4c670a1d8
gonzalotittarelli <gonzalotittarelli@hotmail.com>
2019-03-21 09:49:44 -0300

    advanced

--- region_labeling_concurrent/src/main.rs
+++ region_labeling_concurrent/src/main.rs
@@ -114,40 +114,18 @@ func worker(w int, length int, n int, wg *sync.WaitGroup) {
 	cresult[w] <- locallabel[1:len(locallabel)-1]
 	wg.Done()
 }
-
-func main() {
-	
-	
-	for i := range answer {
-		answer[i] = make(chan bool)
-		first[i] = make(chan [][]int, 200)
-		second[i] = make(chan [][]int, 200)
-		cresult[i] = make(chan [][]int)
-	}
-	start := time.Now()
-	var wg sync.WaitGroup
-	for i := 0; i < P; i++ {
-		wg.Add(1)
-		go worker(i, len(image), len(image[0]), &wg)
-	}
-	coordinator()
-	wg.Wait()
-	log.Printf("--- %s seconds ---", (time.Since(start)))
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}	
-}*/
+*/
 
 use std::fs::File;
 use std::io::{BufReader, BufRead};
 use std::path::Path;
 use std::env;
-use std::time::{Instant};
+//use std::time::{Instant};
 use std::sync::mpsc::{Sender, Receiver};
 use std::sync::mpsc;
 use std::thread;
 
-static P: usize = 3;
+static P: usize = 2;
 const RADIX: u32 = 10;
 
 fn main() {
@@ -190,40 +168,114 @@ fn main() {
     }
     
     let (result_tx, result_rx): (Sender<bool>, Receiver<bool>) = mpsc::channel();
-    let mut answer = &mut Vec::<(Sender<bool>, Receiver<bool>)>::new();
-    let mut first = &mut Vec::<(Sender<Vec<Vec<u64>>>, Receiver<Vec<Vec<u64>>>)>::new();
-    let mut second = &mut Vec::<(Sender<Vec<Vec<u64>>>, Receiver<Vec<Vec<u64>>>)>::new();
+    let mut answer = Vec::<(Sender<bool>, Receiver<bool>)>::new();
+	let first_image = Vec::<(Sender<Vec<Vec<u8>>>, Receiver<Vec<Vec<u8>>>)>::new();
+    let first_label = Vec::<(Sender<Vec<Vec<u64>>>, Receiver<Vec<Vec<u64>>>)>::new();
+    let mut second_image = Vec::<(Sender<Vec<Vec<u8>>>, Receiver<Vec<Vec<u8>>>)>::new();
+	let mut second_label = Vec::<(Sender<Vec<Vec<u64>>>, Receiver<Vec<Vec<u64>>>)>::new();
 
     for _ in 0..P {
         answer.push(mpsc::channel());   
-        first.push(mpsc::channel());
-        second.push(mpsc::channel());
+        first_image.push(mpsc::channel());
+		first_label.push(mpsc::channel());
+        second_image.push(mpsc::channel());
+		second_label.push(mpsc::channel());
 	}
 
-    let (tx, rx): (Sender<i32>, Receiver<i32>) = mpsc::channel();    
+    let (tx_image, rx_image): (Sender<Vec<Vec<u8>>>, Receiver<Vec<Vec<u8>>>) = mpsc::channel();
+	let (tx_label, rx_label): (Sender<Vec<Vec<u64>>>, Receiver<Vec<Vec<u64>>>) = mpsc::channel();
     let mut childrens = Vec::new();
-        
     for w in 0..P {
-        let sender_answer = &answer[w].0.clone();
-        let sender_first_answer = &answer[w+1].0.clone();
-        let r = result_tx.clone();
+		let grid = grid.to_vec();
+		let matrix = matrix.to_vec();
+		let first_sender_image : Sender<Vec<Vec<u8>>>;
+		if w != P - 1 {
+			first_sender_image = first_image[w-1].0.clone();
+		}else{
+			first_sender_image = first_image[w].0.clone();
+		}
+		let first_sender_label : Sender<Vec<Vec<u64>>>;
+		if w != P - 1 {
+			first_sender_label = first_label[w-1].0.clone();
+		}else{
+			first_sender_label = first_label[w].0.clone();
+		}
+		let second_sender_image : Sender<Vec<Vec<u8>>>;		
+		if w != P - 1 {
+			second_sender_image = second_image[w+1].0.clone();
+		}else{
+			second_sender_image = second_image[w].0.clone();
+		}
+		let second_sender_label : Sender<Vec<Vec<u64>>>;		
+		if w != P - 1 {
+			second_sender_label = second_label[w+1].0.clone();
+		}else{
+			second_sender_label = second_label[w].0.clone();
+		}
+		/*let first_receive_image : Receiver<Vec<Vec<u8>>>;
+		if w != P - 1 {
+			first_receive_image = first_image[w-1].1.clone();
+		}else{
+			first_receive_image = first_image[w].1.clone();
+		}
+		let first_receiver_label : Receiver<Vec<Vec<u64>>>;
+		if w != P - 1 {
+			first_receiver_label = first_label[w-1].1.clone();
+		}else{
+			first_receiver_label = first_label[w].1.clone();
+		}
+		let second_receiver_image : Receiver<Vec<Vec<u8>>>;		
+		if w != P - 1 {
+			second_receiver_image = second_image[w+1].1.clone();
+		}else{
+			second_receiver_image = second_image[w].1.clone();
+		}
+		let second_receiver_label : Receiver<Vec<Vec<u64>>>;		
+		if w != P - 1 {
+			second_receiver_label = second_label[w+1].1.clone();
+		}else{
+			second_receiver_label = second_label[w].1.clone();
+		}*/
+
         // Each thread will send its id via the channel
         let child = thread::spawn(move || {
             let strip_size = m/P;
-            let local_image = &mut vec![vec![0; n]; m];
-            r.send(true);
+            let local_image = &mut vec![vec![0; n]; 1];
+			let local_label = &mut vec![vec![0; n]; 1];
+    		let last_element_image = &mut vec![vec![0; n]; 1];
+			let last_element_label = &mut vec![vec![0; n]; 1];
+			local_image.extend_from_slice(&matrix[(w*strip_size)..((w*strip_size)+strip_size)].to_vec());
+			local_image.append(last_element_image);
+			local_label.extend_from_slice(&grid[(w*strip_size)..((w*strip_size)+strip_size)].to_vec());
+			local_label.append(last_element_label);
+			if w != 0 {
+				let f = local_image[1..2].to_vec();
+				//first_sender_image.send(f)
+				first_image[w-1].0.send(f);
+			}
+			if w != P - 1 {
+				let s = local_image[strip_size..strip_size+1].to_vec();
+				second_sender_image.send(s);
+			}
+			if w != P - 1 {
+				
+				//let belowimage = first_receive_image.recv();
+			}			
         });
+
         childrens.push(child);
     }    
 
-    coordinator(result_rx, answer);
+    //coordinator(result_rx, answer);
     
     // Here, all the messages are collected
-    let mut ids = Vec::with_capacity(P as usize);
+    let mut images = Vec::with_capacity(P as usize);
+	let mut labels = Vec::with_capacity(P as usize);
     for _ in 0..P {
         // The `recv` method picks a message from the channel
         // `recv` will block the current thread if there are no messages available
-        ids.push(rx.recv());
+        images.push(rx_image.recv());
+		labels.push(rx_label.recv());
     }
     
     // Wait for the threads to complete any remaining work
@@ -231,8 +283,12 @@ fn main() {
         child.join().expect("oops! the child thread panicked");
     }
 
+	for i in 0..P {
+		println!("image -> {:?}", images[i]);
+		println!("label -> {:?}", labels[i]);
+    }
     // Show the order in which the messages were sent
-    println!("{:?}", ids);
+    
 
     /*let start = Instant::now();
     region_labeling(matrix, m, n, grid);
@@ -242,7 +298,7 @@ fn main() {
     
     //print_matrix(grid)
 }
-
+/*
 fn coordinator(result: Receiver<bool>, answer: &mut Vec<(Sender<bool>, Receiver<bool>)>) {
     let mut chg : bool = true;
     let mut change : bool = true;
@@ -259,7 +315,7 @@ fn coordinator(result: Receiver<bool>, answer: &mut Vec<(Sender<bool>, Receiver<
 	/*for i := 0; i < P; i++ {
 		<-cresult[i]
 	}*/
-}
+}*/
 /*
 fn worker(result: Sender<bool>, answer: &mut Vec<(Sender<bool>, Receiver<bool>)>, first: &mut Vec<(Sender<Vec<Vec<u64>>>, Receiver<Vec<Vec<u64>>>)>,
 second: &mut Vec<(Sender<Vec<Vec<u64>>>, Receiver<Vec<Vec<u64>>>)>, w: i32, length: usize, n: usize) {
@@ -298,8 +354,9 @@ fn max_neighbours(matrix: &Vec<Vec<u64>>, i: usize, j: usize) -> u64 {
         neighbour.push(matrix[i][j + 1]);
     }
     return *neighbour.iter().max().unwrap();
-}
+}*/
 
+/*
 fn region_labeling(matrix : Vec<Vec<u8>>, m : usize, n : usize, g : &mut Vec<Vec<u64>>) {    
     loop {
         let mut change = false;
